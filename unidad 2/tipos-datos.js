66d6a0926c6a261b94ea0926fb10a721e469a114
Eduardo Moreno <eduardo.moreno@unl.edu.ec>
2026-06-12 13:09:21 -0500

    Agregar carpeta node_folder y archivos js

---
+++ unidad 2/tipos-datos.js
@@ -0,0 +1,34 @@
+//primitivos
+let nombre = 'Maria';
+
+
+//numericos
+let edad = 30;
+
+//booleanos
+let esMayorDeEdad = true;
+
+//null
+let noHayValor = null;
+
+//undefined
+let noDefinido;
+
+//symbol Valor unico
+let simboloUnico = Symbol('unico');
+
+// datos gigantes bigint
+let numeroGrande = 2n;
+
+// COMPLETOS
+//objetos
+let carro = {
+    marca: 'Tesla',
+    modelo: 'S'
+};
+
+//arrays
+let frutas = ['manzana', 'banana', 'naranja'];
+
+//funciones
+function saludar(){}
