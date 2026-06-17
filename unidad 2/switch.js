54b331d3f179b91f6cc72761a6e3a64b3a03afc6
Eduardo Moreno <eduardo.moreno@unl.edu.ec>
2026-06-17 11:18:55 -0500

    Activdad en clase(17-06-2026), Eduardo Moreno(Estructura de datos)

---
+++ unidad 2/switch.js
@@ -0,0 +1,17 @@
+let expr = "Mangos"
+switch(expr){
+    case "Mangos":
+    console.log("Los mangos x5 cuestan $1")    
+    break;
+    case "naranjas":
+        console.log("Las naranjas x10 cuestan $1")
+        break;
+    case "Manzanas":
+        console.log("Las manzanas x5 cuestan $1")
+        break;
+    default:
+        console.log("Fruta no disponible")
+        break;
+
+}
+console.log("Quieres comprar algo adicional??")
